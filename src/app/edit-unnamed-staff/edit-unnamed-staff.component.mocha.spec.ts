a52bb651e57aba5ae6a381d0264f82be3567d388
etan221 <eric.tan@auckland.ac.nz>
2023-06-30 11:08:55 +1200

    TDD-003 refined test case

--- src/app/edit-unnamed-staff/edit-unnamed-staff.component.mocha.spec.ts
+++ src/app/edit-unnamed-staff/edit-unnamed-staff.component.mocha.spec.ts
@@ -6,40 +6,66 @@ import { Budget } from '../common/entities/project';
 import { DateUtil as d} from '../common/utils/dateUtil';
 import { Staff } from '../common/entities/people/staff';
 
-describe('RSM-2897 generateInflationPeriod', () => {
+describe('RSM-2897 inflationDates.generateInflationPeriod', () => {
 
-  describe('inflationDates', () => {
+  let inflationDates: Inflation[] = [];
 
-    let str1: string;
+  describe('on future', () => {
+
     let startTime: number = new Date().getMilliseconds();
 
-    beforeEach(() => {
-      str1 = "str1";
+    before(() => {
+
+      let maxSalaryChangePct: number = 4.5;
+      let budget: Budget = BudgetData.budget_2897;
+      let unnamedStaff1: Staff = UnnamedStaffData.unnamedStaff_2897;
+
+      inflationDates = new InflationHelper(1, 2, maxSalaryChangePct, 1, 11).generateInflationPeriod(
+        d.today(),
+        budget.budgetEndDate,
+        unnamedStaff1
+      );
     });
 
-    afterEach(() => {
-      str1 = "end of str1";
+    after(() => {
       let endTime: number = new Date().getMilliseconds();
       console.log(`time spend: ${endTime - startTime} ms`);
     });
 
-    context('when inputs are validate', () => {
-      it("should return array of Inflation", () => {
+    context('when inputs are valid', () => {
 
-        let maxSalaryChangePct: number = 4.5;
-        let budget: Budget = BudgetData.budget_2897;
-        let unnamedStaff1: Staff = UnnamedStaffData.unnamedStaff_2897;
+      it("should return an array of Inflation", () => {
 
-        let inflationDates: Inflation[] = new InflationHelper(1, 2, maxSalaryChangePct, 1, 11).generateInflationPeriod(
-          d.today(),
-          budget.budgetEndDate,
-          unnamedStaff1
-        );
-          expect(inflationDates).not.to.be.null;
+        expect(inflationDates).not.to.be.null;
+        expect(inflationDates).to.be.an('array');
+        expect(inflationDates).to.have.lengthOf.above(0);
+        expect(inflationDates)
+          .to.be.an.instanceof(Array)
+          .and.to.have.lengthOf.above(0)
+          .and.to.have.length(6);
       });
-      it("should return 'str1'", () => {
-          expect(str1).to.be.equals("str1");
+
+      it("should return 6 objects of Inflation", () => {
+        expect(inflationDates).to.have.length(6);
+        expect(inflationDates[0]).to.have.property('inflationDate');
+        expect(inflationDates[0]).to.have.property('incrementPercent');
       });
+
+      it("should return 6 objects of Inflation 2", () => {
+        for (let i=0; i < 6; i++) {
+          expect(inflationDates[i])
+            .that.includes.all.keys(['inflationDate', 'incrementPercent']);
+        }
+      });
+
+      it("data type example", () => {
+        expect('inflationDates').to.be.an('string');
+        expect(123).to.be.an('number');
+        expect(123.23).to.be.an('number');
+        expect(true).to.be.an('boolean');
+        expect(null).to.be.null;
+      });
+
     });
 
   });
